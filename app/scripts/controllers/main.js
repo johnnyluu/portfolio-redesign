123d03ca56985fddd3e1d3e181f802f3f1a8e141
Jonny Lu <Jonny@LuZeqings-MBP-6.lan>
2016-02-10 14:00:21 +1000

    add animation to skills and nav borders

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the repoApp
  */
 angular.module('repoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $interval) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -20,4 +20,23 @@ angular.module('repoApp')
     	var h = $('.landing').height();
     	$('.eye').css('transform', 'translate(' + (((event.pageX - w / 2 ) / (w / 2))*30) + '%,' + (((event.pageY - h / 2) / (h / 2))*30) + '%)');
     });
+
+    var skills = ['UX', 'WEB', 'DESIGN', 'CODING'];
+    var current_skill = 0;
+    var count = 0;
+    $interval(function(){
+    	count = skills[current_skill].length;
+    	if(current_skill < (skills.length - 1)){
+    		current_skill ++;
+    	} else {
+    		current_skill = 0;
+    	}
+    	count = 0;
+    	$interval(function(){
+    		count ++;
+    		$('#scrolltext').html(skills[current_skill].substr(0, count));
+    		// console.log(skills[current_skill].substr(0, count));
+    	}, 100, (skills[current_skill].length))
+    }, 2000);
+    
   });
